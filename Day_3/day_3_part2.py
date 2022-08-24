80031559d0e9cf8e48827f9505083a06f2fbee04
Sooshimi <masonchee93@gmail.com>
2022-08-24 11:14:36 +0100

    Day 3 Part 2 completed

--- Day_3/day_3_part2.py
+++ Day_3/day_3_part2.py
@@ -16,80 +16,73 @@ with open('Day_3/input.txt') as f:
 o2_data = data.copy()
 co2_data = data.copy()
 
+# Get Oxygen Generator Rating number
 i = 0
-
-while len(o2_data) != 1:
+while len(o2_data) != 1: # repeats until one number remains
     zero_total = 0
     one_total = 0
     
     # determine count of zeros and ones for the corresponding index
     for item in o2_data:
-        
-        # if i == len(item):
-        #     break
-
         if item[i] == '0':
             zero_total += 1
         else:
             one_total += 1
     
     # check most common and then keep most common
-    
-    # keep 0's and remove 1's
-    if (zero_total > one_total):
-        for item in o2_data:
-            if item[i] == '1':
-                o2_data.remove(item)
-    
-    # keep 1's and remove 0's
-    elif (zero_total < one_total) or (zero_total == one_total):
-        for item in o2_data:
-            if item[i] == '0':
-                o2_data.remove(item)
+    if (zero_total > one_total): # keep 0's and remove 1's
+        j=0
+        while len(o2_data) > j: # sequencially check all numbers once
+            if o2_data[j][i] == '1': # checks the bit of the number
+                del(o2_data[j])
+            else:
+                # prevent j index from increasing when an item is removed
+                # because when an element is removed, the index of remaining list is reduced by 1
+                # otherwise it would skip the next index
+                j+=1 
+    elif (zero_total < one_total) or (zero_total == one_total): # keep 1's and remove 0's
+        j=0
+        while len(o2_data) > j:
+            if o2_data[j][i] == '0':
+                del(o2_data[j])
+            else:
+                j+=1
 
     i += 1
-    
-    if i == len(item):
-        i = 0
 
+# Get CO2 Generator Rating number
+# Similar function as the while loop for Oxygen Generator Rating number (above)
 i = 0
-
 while len(co2_data) != 1:
-    
     zero_total = 0
     one_total = 0
     
     # determine count of zeros and ones for the corresponding index
     for item in co2_data:
-
         if item[i] == '0':
             zero_total += 1
         else:
             one_total += 1
     
-    # check most common and then keep least common
-    
-    # keep 1's and remove 0's
-    if (zero_total > one_total):
-        for item in co2_data:
-            if item[i] == '0':
-                co2_data.remove(item)
-    
-    # keep 0's and remove 1's
-    elif (zero_total < one_total) or (zero_total == one_total):
-        for item in co2_data:
-            if item[i] == '1':
-                co2_data.remove(item)
+    # check least common and then keep least common
+    if (zero_total > one_total): # keep 1's and remove 0's
+        j=0
+        while len(co2_data) > j:
+            if co2_data[j][i] == '0':
+                del(co2_data[j])
+            else:
+                j+=1
+    elif (zero_total < one_total) or (zero_total == one_total): # keep 0's and remove 1's
+        j=0
+        while len(co2_data) > j:
+            if co2_data[j][i] == '1':
+                del(co2_data[j])
+            else:
+                j+=1
 
     i += 1
-    
-    if i == len(item):
-        i = 0
 
-print(o2_data)
-print(co2_data)
-
-# convert gamma/epsilon arrays to string (binary numbers)
+# convert arrays to string (binary numbers)
 o2_gen_rating = str(o2_data).replace("'",'').replace('[', '').replace(']', '').replace(' ','')
 co2_gen_rating = str(co2_data).replace("'",'').replace('[', '').replace(']', '').replace(' ','')
 
@@ -99,4 +92,6 @@ co2_gen_rating = int(co2_gen_rating, 2)
 
 life_support_rating = o2_gen_rating * co2_gen_rating
 
+print("Oxygen Generator Rating: ", o2_data, "= ", o2_gen_rating)
+print("CO2 Generator Rating: ", co2_data, "= ", co2_gen_rating)
 print("Life Support Rating = ", life_support_rating)
